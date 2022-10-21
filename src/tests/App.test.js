972702c1f9dd74f03145330392fa967863e50b38
caiobacode <caioodsf@gmail.com>
2022-10-21 18:02:19 -0300

    req5 inutil por enquanto

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import userEvent from '@testing-library/user-event';
 
 test('I am your test', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
+  const linkElement = screen.getByText(/Name/i);
+  const filterButton = screen.getByTestId('button-filter')
+  const nameFilter = screen.getByTestId('name-filter')
+  expect(nameFilter).toBeInTheDocument()
+  userEvent.click(filterButton)
+  expect(filterButton).toBeInTheDocument();
+  userEvent.click(nameFilter)
+  userEvent.keyboard('a')
+  userEvent.click(filterButton)
   expect(linkElement).toBeInTheDocument();
 });
